690fdb7a584bbb246593cf34270390fb6edbc63e
Ezra <Ezra.cabal@utp.ac.pa>
2023-05-15 14:54:51 -0500

    Finished ProgresBar app

--- ProgressBarActivity/app/src/main/java/com/example/progressbaractivity/MainActivity.java
+++ ProgressBarActivity/app/src/main/java/com/example/progressbaractivity/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.progressbaractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +27,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 progressBar.setProgress(Integer.parseInt(numberInput.getText().toString()));
+            }
+        });
+
+        //Datepicker
+        final TextView showdate=(TextView)findViewById(R.id.tvdate);
+        final DatePicker picker = (DatePicker) findViewById(R.id.datePiker1);
+        Button btndate = (Button) findViewById(R.id.showdate);
+
+        btndate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                showdate.setText("Selected Date: "+ picker.getDayOfMonth()+"/"+ (picker.getMonth() + 1)+"/"+picker.getYear());
+            }
+        });
+
+        //timepicker
+        final TextView showtime=(TextView)findViewById(R.id.textview_time);
+        final TimePicker tpicker = (TimePicker) findViewById(R.id.timePiker1);
+        Button btntime = (Button) findViewById(R.id.btn_showtime);
+
+        btntime.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                showtime.setText("Selected Time: "+ tpicker.getHour()+":"+ tpicker.getMinute());
             }
         });
     }
